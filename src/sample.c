d4495ea7af6f594c32d6d00b33ba7d7fce97f00a
jason-devcode <jheisontoro62@gmail.com>
2024-07-30 01:53:15 -0500

    feat: update sample

--- src/sample.c
+++ src/sample.c
@@ -1,19 +1,6 @@
 #include "../include/graphic_engine.h" // Includes the custom graphics engine header
-
-// use r*r = x*x + y*y circle formula for find points inside the circle
-void drawCircle(int cx, int cy, int radius, uint32_t color)
-{
-    for (int Y = -radius; Y < radius; ++Y)
-    {
-        for (int X = -radius; X < radius; ++X)
-        {
-            if ((X * X + Y * Y) < radius * radius) // if the point is inside of the circle
-            {
-                pixel(X+cx, Y+cy, color);
-            }
-        }
-    }
-}
+#include "../include/geometry.h"
+#include "../include/utils/color.h"
 
 /**
  * The game loop function that continuously updates the screen.
@@ -27,12 +14,12 @@ void *gameLoop(void *arg)
     {
         clearScreen(); // Clears the screen with a black color
 
-        drawCircle( 256, 256, 64, 0xFFFF0000 );
+        drawCircle(256, 256, 64, RGB(255,0,0));
 
-        drawScreen();  // Update the screen with the current framebuffer contents
-        SDL_Delay(16); // Delay to maintain approximately 60 FPS
-    } while (true); // Infinite loop to keep the game running
-    return NULL; // Return NULL as required by the thread function signature
+        drawScreen();
+        SDL_Delay(16); 
+    } while (ON_GAME_RUNNING); 
+    return NULL;
 }
 
 /**
