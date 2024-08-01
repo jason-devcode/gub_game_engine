2326820dac8d9a1d8ae110d3ac28fefc1e4f3749
jason-devcode <jheisontoro62@gmail.com>
2024-08-01 00:06:09 -0500

    feat: added trigonometry utils

---
+++ include/utils/trigonometry.h
@@ -0,0 +1,18 @@
+#ifndef TRIGONOMETRY_H
+#define TRIGONOMETRY_H
+
+
+#define HALFPI (1.5707963f)
+
+#define PI (3.1415927f)
+
+#define TAU (6.2831853f) // Same to PI * 2
+
+#define radian (0.01745329251f)
+
+#define radiansToDegrees(radians) ((radians) * (57.2957795131f))
+
+#define degreesToRadians(degrees) ((degrees) * radian)
+
+
+#endif
