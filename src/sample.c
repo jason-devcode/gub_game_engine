c1baa61fbd0eff7660b75a796977c0a22655b52d
jason-devcode <jheisontoro62@gmail.com>
2024-08-01 00:07:41 -0500

    feat: update sample file

--- src/sample.c
+++ src/sample.c
@@ -1,45 +1,33 @@
-#include "../include/graphic_engine.h" // Includes the custom graphics engine header
-#include "../include/geometry.h"
+#include "../include/graphic_engine.h"
+#include "../include/utils/geometry.h"
+#include "../include/utils/font.h"
 #include "../include/utils/color.h"
 
-/**
- * The game loop function that continuously updates the screen.
- *
- * @param arg A pointer to any arguments passed to the thread (not used here).
- * @return Returns NULL (the thread function return type must be void*).
- */
 void *gameLoop(void *arg)
 {
     do
     {
-        clearScreen(); // Clears the screen with a black color
+        clearScreen();
 
-        drawCircle(256, 256, 64, RGB(255,0,0));
+        drawFormattedText(
+            10, 10, 0xFFFFFF00,
+            "FPS: %d\ndeltatime: %lf",
+            getFps(), getDeltatime());
 
         drawScreen();
-        SDL_Delay(16); 
-    } while (ON_GAME_RUNNING); 
+
+        renderDelay(16); // Limit to 60  FPS
+    } while (ON_GAME_RUNNING);
+
     return NULL;
 }
 
-/**
- * Main function to initialize, run, and clean up the graphics engine.
- *
- * @return Returns EXIT_SUCCESS to indicate successful completion.
- */
 int main()
 {
-    // Initialize the graphics engine with a screen size of 512x512 and a window title
-    initGraphicEngine(512, 512, "Basic SDL Engine");
-
-    // Initialize the game loop with the gameLoop function
+    // 4:3 screen ratio size
+    initGraphicEngine(640, 480, "Basic SDL Engine");
     initializeGameLoop(gameLoop);
-
-    // Run the main engine loop, which handles events and updates the screen
     runEngine();
-
-    // Clean up resources and shut down the engine
     clearEngine();
-
-    return EXIT_SUCCESS; // Return success exit code
+    return EXIT_SUCCESS;
 }
