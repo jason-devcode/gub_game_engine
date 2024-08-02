56e5297587e22c20ad977e8c8899a9f19e59345c
jason-devcode <jheisontoro62@gmail.com>
2024-08-02 00:48:02 -0500

    feat: added mouse events manager

---
+++ include/managers/mouse_event_manager.h
@@ -0,0 +1,54 @@
+#ifndef MOUSE_EVENT_MANAGER_H
+#define MOUSE_EVENT_MANAGER_H
+
+#include "event_manager.h"
+
+/**
+ * @enum MouseEventType
+ * @brief Enumeration of possible mouse event types.
+ *
+ * This enumeration defines the different types of mouse events that can be handled
+ * by the EventManager.
+ */
+typedef enum
+{
+    MOUSE_MOVE,               /**< Event for mouse movement. */
+    MOUSE_LEFT_BUTTON_DOWN,   /**< Event for mouse left button press. */
+    MOUSE_MIDDLE_BUTTON_DOWN, /**< Event for mouse middle button press. */
+    MOUSE_RIGHT_BUTTON_DOWN,  /**< Event for mouse right button press. */
+    MOUSE_WHELL_BUTTON_DOWN,  /**< Event for mouse wheel down. */
+    MOUSE_WHELL_BUTTON_UP,    /**< Event for mouse wheel up. */
+    MOUSE_LEFT_BUTTON_UP,     /**< Event for mouse left button release. */
+    MOUSE_MIDDLE_BUTTON_UP,   /**< Event for mouse middle button release. */
+    MOUSE_RIGHT_BUTTON_UP,    /**< Event for mouse right button release. */
+    MOUSE_LAST
+} MouseEventType;
+
+// Global EventManager for mouse events
+EventManager gMouseEventManager;
+
+/**
+ * @brief Adds a mouse event listener.
+ *
+ * This macro registers a listener for a specific type of mouse event.
+ *
+ * @param type The type of mouse event to listen for.
+ * @param listener The function to call when the event is triggered.
+ * @return true if the listener was successfully added, false otherwise.
+ */
+#define addMouseEventListener(type, listener) \
+    addEventListener(&gMouseEventManager, (uint32_t)(type), (listener))
+
+/**
+ * @brief Removes a mouse event listener.
+ *
+ * This macro removes a listener for a specific type of mouse event.
+ *
+ * @param type The type of mouse event to stop listening for.
+ * @param listener The function to remove.
+ * @return true if the listener was successfully removed, false otherwise.
+ */
+#define removeMouseEventListener(type, listener) \
+    removeEventListener(&gMouseEventManager, (uint32_t)(type), (listener))
+
+#endif
---
+++ include/utils/mouse.h
@@ -0,0 +1,35 @@
+#ifndef MOUSE_UTILS_H
+#define MOUSE_UTILS_H
+
+#include "../managers/mouse_event_manager.h"
+
+/**
+ * @struct MouseState
+ * @brief Structure to keep track of the mouse's state.
+ *
+ * This structure holds global data about the mouse, such as its position and button states.
+ */
+typedef struct
+{
+    int x; /**< The x-coordinate of the mouse cursor. */
+    int y; /**< The y-coordinate of the mouse cursor. */
+} MouseState;
+
+// Global instance of MouseState
+MouseState gMouseState = {0, 0};
+
+/**
+ * @brief Macro to get the X position of the mouse.
+ *
+ * @return The X position of the mouse.
+ */
+#define GET_MOUSE_X() (gMouseState.x)
+
+/**
+ * @brief Macro to get the Y position of the mouse.
+ *
+ * @return The Y position of the mouse.
+ */
+#define GET_MOUSE_Y() (gMouseState.y)
+
+#endif // MOUSE_UTILS_H
