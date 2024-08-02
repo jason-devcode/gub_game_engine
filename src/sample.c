0cdae81cbb0cef5921ec8bdbe4287c75a0cd53f8
jason-devcode <jheisontoro62@gmail.com>
2024-08-02 00:48:56 -0500

    feat: update sample

--- src/sample.c
+++ src/sample.c
@@ -32,12 +32,33 @@ void onPlayerRotateLeft()
     playerPosition.x -= getDeltatime() * 0.001;
 }
 
+void onLeftUpClick()
+{
+    puts("Solto izquierda");
+}
+
+void onRightUpClick()
+{
+    puts("Solto derecha");
+}
+
+void onCursorMotion()
+{
+    puts("Moviendo mouse");
+}
+
 void *gameLoop(void *arg)
 {
     addKeyPressEventListener(SDLK_w, onPlayerMoveForward);
     addKeyPressEventListener(SDLK_s, onPlayerMoveBackward);
     addKeyPressEventListener(SDLK_a, onPlayerRotateLeft);
     addKeyPressEventListener(SDLK_d, onPlayerRotateRight);
+
+    addMouseEventListener(MOUSE_MOVE, onCursorMotion);
+    addMouseEventListener(MOUSE_LEFT_BUTTON_DOWN, onPlayerRotateLeft);
+    addMouseEventListener(MOUSE_RIGHT_BUTTON_DOWN, onPlayerRotateRight);
+    addMouseEventListener(MOUSE_LEFT_BUTTON_UP, onLeftUpClick);
+    addMouseEventListener(MOUSE_RIGHT_BUTTON_UP, onRightUpClick);
 
     do
     {
@@ -57,4 +78,4 @@ void *gameLoop(void *arg)
     return NULL;
 }
 
-CREATE_GAME( 640, 480, "Basic Game Engine" );
+CREATE_GAME(640, 480, "Basic Game Engine");
