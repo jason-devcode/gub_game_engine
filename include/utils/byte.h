c80c5a820a45d1af6a629bca586fee57b8349c61
jason-devcode <jheisontoro62@gmail.com>
2024-08-03 03:09:10 -0500

    feat: added byte management types

--- include/utils/byte.h
+++ include/utils/byte.h
@@ -1,8 +1,26 @@
 #ifndef BYTE_UTILS
 #define BYTE_UTILS
 
+#include <stdint.h>
+
 #define CLEAR_BYTE(byte) (byte & 0xFF)
 #define INSERT_BYTE(byte, byte_position) (CLEAR_BYTE(byte) << (byte_position << 3))
 #define EXTRACT_BYTE(Value4Bytes, byte_position) ((Value4Bytes >> (byte_position << 3) & 0xFF))
+
+#ifndef BYTE
+typedef uint8_t BYTE, Byte;
+#endif
+
+#ifndef WORD
+typedef uint16_t WORD, Word;
+#endif
+
+#ifndef DWORD
+typedef uint32_t DWORD, Dword;
+#endif
+
+#ifndef QWORD
+typedef uint64_t QWORD, Qword;
+#endif
 
 #endif
