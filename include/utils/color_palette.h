0dff6f59a70c2788e849fd3696552ce0a896e4c0
jason-devcode <jheisontoro62@gmail.com>
2024-08-04 11:32:30 -0500

    refactor: extracted color palette to a individual header file

---
+++ include/utils/color_palette.h
@@ -0,0 +1,417 @@
+#ifndef COLOR_PALETTE_UTILS_H
+#define COLOR_PALETTE_UTILS_H
+
+#include "color.h"
+
+#define RED RGB(255, 0, 0)
+#define GREEN RGB(0, 255, 0)
+#define BLUE RGB(0, 0, 255)
+#define YELLOW RGB(255, 255, 0)
+
+#define WHITE RGB(255, 255, 255)
+#define BLACK RGB(0, 0, 0)
+
+// Colors palette extracted from tailwind css
+
+// Slate
+#define SLATE_50 RGB(248, 250, 252)
+#define SLATE_100 RGB(241, 245, 249)
+#define SLATE_200 RGB(226, 232, 240)
+#define SLATE_300 RGB(203, 213, 225)
+#define SLATE_400 RGB(148, 163, 184)
+#define SLATE_500 RGB(100, 116, 139)
+#define SLATE_600 RGB(71, 85, 105)
+#define SLATE_700 RGB(51, 65, 85)
+#define SLATE_800 RGB(30, 41, 59)
+#define SLATE_900 RGB(15, 23, 42)
+#define SLATE_950 RGB(2, 6, 23)
+
+// Gray
+#define GRAY_50 RGB(249, 250, 251)
+#define GRAY_100 RGB(243, 244, 246)
+#define GRAY_200 RGB(229, 231, 235)
+#define GRAY_300 RGB(209, 213, 219)
+#define GRAY_400 RGB(156, 163, 175)
+#define GRAY_500 RGB(107, 114, 128)
+#define GRAY_600 RGB(75, 85, 99)
+#define GRAY_700 RGB(55, 65, 81)
+#define GRAY_800 RGB(31, 41, 55)
+#define GRAY_900 RGB(17, 24, 39)
+#define GRAY_950 RGB(3, 7, 18)
+
+// Zinc
+#define ZINC_50 RGB(250, 250, 250)
+#define ZINC_100 RGB(244, 244, 245)
+#define ZINC_200 RGB(228, 228, 231)
+#define ZINC_300 RGB(212, 212, 216)
+#define ZINC_400 RGB(161, 161, 170)
+#define ZINC_500 RGB(113, 113, 122)
+#define ZINC_600 RGB(82, 82, 91)
+#define ZINC_700 RGB(63, 63, 70)
+#define ZINC_800 RGB(39, 39, 42)
+#define ZINC_900 RGB(24, 24, 27)
+#define ZINC_950 RGB(9, 9, 11)
+
+// Neutral
+#define NEUTRAL_50 RGB(250, 250, 250)
+#define NEUTRAL_100 RGB(245, 245, 245)
+#define NEUTRAL_200 RGB(229, 229, 229)
+#define NEUTRAL_300 RGB(212, 212, 212)
+#define NEUTRAL_400 RGB(163, 163, 163)
+#define NEUTRAL_500 RGB(115, 115, 115)
+#define NEUTRAL_600 RGB(82, 82, 82)
+#define NEUTRAL_700 RGB(64, 64, 64)
+#define NEUTRAL_800 RGB(38, 38, 38)
+#define NEUTRAL_900 RGB(23, 23, 23)
+#define NEUTRAL_950 RGB(10, 10, 10)
+
+// Stone
+#define STONE_50 RGB(250, 250, 249)
+#define STONE_100 RGB(245, 245, 244)
+#define STONE_200 RGB(231, 229, 228)
+#define STONE_300 RGB(214, 211, 209)
+#define STONE_400 RGB(168, 162, 158)
+#define STONE_500 RGB(120, 113, 108)
+#define STONE_600 RGB(87, 83, 78)
+#define STONE_700 RGB(68, 64, 60)
+#define STONE_800 RGB(41, 37, 36)
+#define STONE_900 RGB(28, 25, 23)
+#define STONE_950 RGB(12, 10, 9)
+
+// Red
+#define RED_50 RGB(254, 242, 242)
+#define RED_100 RGB(254, 226, 226)
+#define RED_200 RGB(254, 202, 202)
+#define RED_300 RGB(252, 165, 165)
+#define RED_400 RGB(248, 113, 113)
+#define RED_500 RGB(239, 68, 68)
+#define RED_600 RGB(220, 38, 38)
+#define RED_700 RGB(185, 28, 28)
+#define RED_800 RGB(153, 27, 27)
+#define RED_900 RGB(127, 29, 29)
+#define RED_950 RGB(69, 10, 10)
+
+// Orange
+#define ORANGE_50 RGB(255, 247, 237)
+#define ORANGE_100 RGB(255, 237, 213)
+#define ORANGE_200 RGB(254, 215, 170)
+#define ORANGE_300 RGB(253, 186, 116)
+#define ORANGE_400 RGB(251, 146, 60)
+#define ORANGE_500 RGB(249, 115, 22)
+#define ORANGE_600 RGB(234, 88, 12)
+#define ORANGE_700 RGB(194, 65, 12)
+#define ORANGE_800 RGB(154, 52, 18)
+#define ORANGE_900 RGB(124, 45, 18)
+#define ORANGE_950 RGB(67, 20, 7)
+
+// Amber
+#define AMBER_50 RGB(255, 251, 235)
+#define AMBER_100 RGB(254, 243, 199)
+#define AMBER_200 RGB(253, 230, 138)
+#define AMBER_300 RGB(252, 211, 77)
+#define AMBER_400 RGB(251, 191, 36)
+#define AMBER_500 RGB(245, 158, 11)
+#define AMBER_600 RGB(217, 119, 6)
+#define AMBER_700 RGB(180, 83, 9)
+#define AMBER_800 RGB(146, 64, 14)
+#define AMBER_900 RGB(120, 53, 15)
+#define AMBER_950 RGB(69, 26, 3)
+
+// Yellow
+#define YELLOW_50 RGB(254, 252, 232)
+#define YELLOW_100 RGB(254, 249, 195)
+#define YELLOW_200 RGB(254, 240, 138)
+#define YELLOW_300 RGB(253, 224, 71)
+#define YELLOW_400 RGB(250, 204, 21)
+#define YELLOW_500 RGB(234, 179, 8)
+#define YELLOW_600 RGB(202, 138, 4)
+#define YELLOW_700 RGB(161, 98, 7)
+#define YELLOW_800 RGB(133, 77, 14)
+#define YELLOW_900 RGB(113, 63, 18)
+#define YELLOW_950 RGB(66, 32, 6)
+
+// Lime
+#define LIME_50 RGB(247, 254, 231)
+#define LIME_100 RGB(236, 252, 203)
+#define LIME_200 RGB(217, 249, 157)
+#define LIME_300 RGB(190, 242, 100)
+#define LIME_400 RGB(163, 230, 53)
+#define LIME_500 RGB(132, 204, 22)
+#define LIME_600 RGB(101, 163, 13)
+#define LIME_700 RGB(77, 124, 15)
+#define LIME_800 RGB(63, 98, 18)
+#define LIME_900 RGB(54, 83, 20)
+#define LIME_950 RGB(26, 46, 5)
+
+// Green
+#define GREEN_50 RGB(240, 253, 244)
+#define GREEN_100 RGB(220, 252, 231)
+#define GREEN_200 RGB(187, 247, 208)
+#define GREEN_300 RGB(134, 239, 172)
+#define GREEN_400 RGB(74, 222, 128)
+#define GREEN_500 RGB(34, 197, 94)
+#define GREEN_600 RGB(22, 163, 74)
+#define GREEN_700 RGB(21, 128, 61)
+#define GREEN_800 RGB(22, 101, 52)
+#define GREEN_900 RGB(20, 83, 45)
+#define GREEN_950 RGB(5, 46, 22)
+
+// Emerald
+#define EMERALD_50 RGB(236, 253, 245)
+#define EMERALD_100 RGB(209, 250, 229)
+#define EMERALD_200 RGB(167, 243, 208)
+#define EMERALD_300 RGB(110, 231, 183)
+#define EMERALD_400 RGB(52, 211, 153)
+#define EMERALD_500 RGB(16, 185, 129)
+#define EMERALD_600 RGB(5, 150, 105)
+#define EMERALD_700 RGB(4, 120, 87)
+#define EMERALD_800 RGB(6, 95, 70)
+#define EMERALD_900 RGB(6, 78, 59)
+#define EMERALD_950 RGB(2, 44, 34)
+
+// Teal
+#define TEAL_50 RGB(240, 253, 250)
+#define TEAL_100 RGB(204, 251, 241)
+#define TEAL_200 RGB(153, 246, 228)
+#define TEAL_300 RGB(94, 234, 212)
+#define TEAL_400 RGB(45, 212, 191)
+#define TEAL_500 RGB(20, 184, 166)
+#define TEAL_600 RGB(13, 148, 136)
+#define TEAL_700 RGB(15, 118, 110)
+#define TEAL_800 RGB(17, 94, 89)
+#define TEAL_900 RGB(19, 78, 74)
+#define TEAL_950 RGB(4, 47, 46)
+
+// Cyan
+#define CYAN_50 RGB(236, 254, 255)
+#define CYAN_100 RGB(207, 250, 254)
+#define CYAN_200 RGB(165, 243, 252)
+#define CYAN_300 RGB(103, 232, 249)
+#define CYAN_400 RGB(34, 211, 238)
+#define CYAN_500 RGB(6, 182, 212)
+#define CYAN_600 RGB(8, 145, 178)
+#define CYAN_700 RGB(14, 116, 144)
+#define CYAN_800 RGB(21, 94, 117)
+#define CYAN_900 RGB(22, 78, 99)
+#define CYAN_950 RGB(8, 51, 68)
+
+// Sky
+#define SKY_50 RGB(240, 249, 255)
+#define SKY_100 RGB(224, 242, 254)
+#define SKY_200 RGB(186, 230, 253)
+#define SKY_300 RGB(125, 211, 252)
+#define SKY_400 RGB(56, 189, 248)
+#define SKY_500 RGB(14, 165, 233)
+#define SKY_600 RGB(2, 132, 199)
+#define SKY_700 RGB(3, 105, 161)
+#define SKY_800 RGB(7, 89, 133)
+#define SKY_900 RGB(12, 74, 110)
+#define SKY_950 RGB(8, 47, 73)
+
+// Blue
+#define BLUE_50 RGB(239, 246, 255)
+#define BLUE_100 RGB(219, 234, 254)
+#define BLUE_200 RGB(191, 219, 254)
+#define BLUE_300 RGB(147, 197, 253)
+#define BLUE_400 RGB(96, 165, 250)
+#define BLUE_500 RGB(59, 130, 246)
+#define BLUE_600 RGB(37, 99, 235)
+#define BLUE_700 RGB(29, 78, 216)
+#define BLUE_800 RGB(30, 64, 159)
+#define BLUE_900 RGB(30, 58, 138)
+#define BLUE_950 RGB(23, 37, 84)
+
+// Indigo
+#define INDIGO_50 RGB(238, 242, 255)
+#define INDIGO_100 RGB(224, 231, 255)
+#define INDIGO_200 RGB(199, 210, 254)
+#define INDIGO_300 RGB(165, 180, 252)
+#define INDIGO_400 RGB(129, 140, 248)
+#define INDIGO_500 RGB(99, 102, 241)
+#define INDIGO_600 RGB(79, 70, 229)
+#define INDIGO_700 RGB(67, 56, 202)
+#define INDIGO_800 RGB(55, 48, 163)
+#define INDIGO_900 RGB(49, 46, 129)
+#define INDIGO_950 RGB(30, 27, 75)
+
+// Violet
+#define VIOLET_50 RGB(245, 243, 255)
+#define VIOLET_100 RGB(237, 233, 254)
+#define VIOLET_200 RGB(221, 214, 254)
+#define VIOLET_300 RGB(196, 181, 253)
+#define VIOLET_400 RGB(167, 139, 250)
+#define VIOLET_500 RGB(139, 92, 246)
+#define VIOLET_600 RGB(124, 58, 237)
+#define VIOLET_700 RGB(109, 28, 217)
+#define VIOLET_800 RGB(91, 33, 182)
+#define VIOLET_900 RGB(76, 29, 149)
+#define VIOLET_950 RGB(46, 16, 101)
+
+// Purple
+#define PURPLE_50 RGB(250, 245, 255)
+#define PURPLE_100 RGB(243, 232, 255)
+#define PURPLE_200 RGB(233, 213, 255)
+#define PURPLE_300 RGB(216, 180, 254)
+#define PURPLE_400 RGB(192, 132, 254)
+#define PURPLE_500 RGB(168, 85, 247)
+#define PURPLE_600 RGB(147, 51, 234)
+#define PURPLE_700 RGB(126, 34, 206)
+#define PURPLE_800 RGB(107, 33, 168)
+#define PURPLE_900 RGB(88, 28, 135)
+#define PURPLE_950 RGB(59, 7, 100)
+
+// Fuchsia
+#define FUCHSIA_50 RGB(253, 244, 255)
+#define FUCHSIA_100 RGB(250, 232, 255)
+#define FUCHSIA_200 RGB(245, 208, 254)
+#define FUCHSIA_300 RGB(240, 171, 252)
+#define FUCHSIA_400 RGB(232, 121, 249)
+#define FUCHSIA_500 RGB(217, 70, 239)
+#define FUCHSIA_600 RGB(192, 38, 211)
+#define FUCHSIA_700 RGB(162, 28, 175)
+#define FUCHSIA_800 RGB(134, 25, 143)
+#define FUCHSIA_900 RGB(112, 26, 117)
+#define FUCHSIA_950 RGB(74, 4, 78)
+
+// Pink
+#define PINK_50 RGB(253, 242, 248)
+#define PINK_100 RGB(252, 231, 243)
+#define PINK_200 RGB(251, 207, 232)
+#define PINK_300 RGB(249, 168, 212)
+#define PINK_400 RGB(244, 114, 182)
+#define PINK_500 RGB(236, 72, 153)
+#define PINK_600 RGB(219, 39, 119)
+#define PINK_700 RGB(190, 24, 93)
+#define PINK_800 RGB(157, 23, 77)
+#define PINK_900 RGB(131, 19, 67)
+#define PINK_950 RGB(80, 7, 36)
+
+// Rose
+#define ROSE_50 RGB(255, 241, 242)
+#define ROSE_100 RGB(255, 228, 230)
+#define ROSE_200 RGB(254, 205, 211)
+#define ROSE_300 RGB(253, 164, 175)
+#define ROSE_400 RGB(251, 113, 133)
+#define ROSE_500 RGB(244, 63, 94)
+#define ROSE_600 RGB(225, 29, 72)
+#define ROSE_700 RGB(190, 18, 60)
+#define ROSE_800 RGB(159, 18, 57)
+#define ROSE_900 RGB(136, 19, 55)
+#define ROSE_950 RGB(76, 5, 25)
+
+#define COLOR_COUNT 24
+#define SHADE_COUNT 11
+
+const unsigned int colors_tailwind_palette[COLOR_COUNT][SHADE_COUNT] = {
+    // Slate
+    {
+        SLATE_50, SLATE_100, SLATE_200, SLATE_300, SLATE_400,
+        SLATE_500, SLATE_600, SLATE_700, SLATE_800, SLATE_900,
+        SLATE_950},
+    // Gray
+    {
+        GRAY_50, GRAY_100, GRAY_200, GRAY_300, GRAY_400,
+        GRAY_500, GRAY_600, GRAY_700, GRAY_800, GRAY_900,
+        GRAY_950},
+    // Zinc
+    {
+        ZINC_50, ZINC_100, ZINC_200, ZINC_300, ZINC_400,
+        ZINC_500, ZINC_600, ZINC_700, ZINC_800, ZINC_900,
+        ZINC_950},
+    // Neutral
+    {
+        NEUTRAL_50, NEUTRAL_100, NEUTRAL_200, NEUTRAL_300, NEUTRAL_400,
+        NEUTRAL_500, NEUTRAL_600, NEUTRAL_700, NEUTRAL_800, NEUTRAL_900,
+        NEUTRAL_950},
+    // Stone
+    {
+        STONE_50, STONE_100, STONE_200, STONE_300, STONE_400,
+        STONE_500, STONE_600, STONE_700, STONE_800, STONE_900,
+        STONE_950},
+    // Red
+    {
+        RED_50, RED_100, RED_200, RED_300, RED_400,
+        RED_500, RED_600, RED_700, RED_800, RED_900,
+        RED_950},
+    // Orange
+    {
+        ORANGE_50, ORANGE_100, ORANGE_200, ORANGE_300, ORANGE_400,
+        ORANGE_500, ORANGE_600, ORANGE_700, ORANGE_800, ORANGE_900,
+        ORANGE_950},
+    // Amber
+    {
+        AMBER_50, AMBER_100, AMBER_200, AMBER_300, AMBER_400,
+        AMBER_500, AMBER_600, AMBER_700, AMBER_800, AMBER_900,
+        AMBER_950},
+    // Yellow
+    {
+        YELLOW_50, YELLOW_100, YELLOW_200, YELLOW_300, YELLOW_400,
+        YELLOW_500, YELLOW_600, YELLOW_700, YELLOW_800, YELLOW_900,
+        YELLOW_950},
+    // Lime
+    {
+        LIME_50, LIME_100, LIME_200, LIME_300, LIME_400,
+        LIME_500, LIME_600, LIME_700, LIME_800, LIME_900,
+        LIME_950},
+    // Green
+    {
+        GREEN_50, GREEN_100, GREEN_200, GREEN_300, GREEN_400,
+        GREEN_500, GREEN_600, GREEN_700, GREEN_800, GREEN_900,
+        GREEN_950},
+    // Emerald
+    {
+        EMERALD_50, EMERALD_100, EMERALD_200, EMERALD_300, EMERALD_400,
+        EMERALD_500, EMERALD_600, EMERALD_700, EMERALD_800, EMERALD_900,
+        EMERALD_950},
+    // Teal
+    {
+        TEAL_50, TEAL_100, TEAL_200, TEAL_300, TEAL_400,
+        TEAL_500, TEAL_600, TEAL_700, TEAL_800, TEAL_900,
+        TEAL_950},
+    // Cyan
+    {
+        CYAN_50, CYAN_100, CYAN_200, CYAN_300, CYAN_400,
+        CYAN_500, CYAN_600, CYAN_700, CYAN_800, CYAN_900,
+        CYAN_950},
+    // Sky
+    {
+        SKY_50, SKY_100, SKY_200, SKY_300, SKY_400,
+        SKY_500, SKY_600, SKY_700, SKY_800, SKY_900,
+        SKY_950},
+    // Blue
+    {
+        BLUE_50, BLUE_100, BLUE_200, BLUE_300, BLUE_400,
+        BLUE_500, BLUE_600, BLUE_700, BLUE_800, BLUE_900,
+        BLUE_950},
+    // Indigo
+    {
+        INDIGO_50, INDIGO_100, INDIGO_200, INDIGO_300, INDIGO_400,
+        INDIGO_500, INDIGO_600, INDIGO_700, INDIGO_800, INDIGO_900,
+        INDIGO_950},
+    // Violet
+    {
+        VIOLET_50, VIOLET_100, VIOLET_200, VIOLET_300, VIOLET_400,
+        VIOLET_500, VIOLET_600, VIOLET_700, VIOLET_800, VIOLET_900,
+        VIOLET_950},
+    // Purple
+    {
+        PURPLE_50, PURPLE_100, PURPLE_200, PURPLE_300, PURPLE_400,
+        PURPLE_500, PURPLE_600, PURPLE_700, PURPLE_800, PURPLE_900,
+        PURPLE_950},
+    // Fuchsia
+    {
+        FUCHSIA_50, FUCHSIA_100, FUCHSIA_200, FUCHSIA_300, FUCHSIA_400,
+        FUCHSIA_500, FUCHSIA_600, FUCHSIA_700, FUCHSIA_800, FUCHSIA_900,
+        FUCHSIA_950},
+    // Pink
+    {
+        PINK_50, PINK_100, PINK_200, PINK_300, PINK_400,
+        PINK_500, PINK_600, PINK_700, PINK_800, PINK_900,
+        PINK_950},
+    // Rose
+    {
+        ROSE_50, ROSE_100, ROSE_200, ROSE_300, ROSE_400,
+        ROSE_500, ROSE_600, ROSE_700, ROSE_800, ROSE_900,
+        ROSE_950}};
+
+#endif
