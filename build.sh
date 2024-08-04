cdb87139d4e5f694b7afb04ecb3f184a607ce31c
jason-devcode <jheisontoro62@gmail.com>
2024-08-04 18:56:56 -0500

    fix: fixed build script

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ if [ ! -d "build" ]; then
     mkdir build
 fi
 
-gcc src/sample_raster.c -o build/sample -lm -lSDL -O3
+gcc src/sample_parallelogram.c -o build/sample -lm -lSDL -O3
 
 echo "Compilation completed."
