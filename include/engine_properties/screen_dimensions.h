1361d4425202f9da305200145ec21328753fdd8c
jason-devcode <jheisontoro62@gmail.com>
2024-08-04 01:58:11 -0500

    feat: added half screen dimensions

--- include/engine_properties/screen_dimensions.h
+++ include/engine_properties/screen_dimensions.h
@@ -8,6 +8,9 @@ uint32_t gScreenWidth = 0;       // Width of the screen in pixels
 uint32_t gScreenHeight = 0;      // Height of the screen in pixels
 uint32_t gScreenTotalPixels = 0; // Total pixels in the screen
 
+#define gHalfScreenWidth (gScreenWidth >> 1)  // Half the screen width in pixels
+#define gHalfScreenHeight (gScreenHeight >> 1)  // Half the screen height in pixels
+
 // Corrected pixel width for handling SDL screen pitch
 uint32_t gCorrectPixelsWidth = 0; // Number of pixels per row in framebuffer
 
