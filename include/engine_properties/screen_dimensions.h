aea8996d3a638018b9d3ef3d0aaa977aad0d97ef
jason-devcode <jheisontoro62@gmail.com>
2024-08-04 22:54:08 -0500

    chore!: change screen dimensions unsigned type to signed type for avoid render problems

--- include/engine_properties/screen_dimensions.h
+++ include/engine_properties/screen_dimensions.h
@@ -4,14 +4,14 @@
 #include <stdint.h>
 
 // Global screen dimensions
-uint32_t gScreenWidth = 0;       // Width of the screen in pixels
-uint32_t gScreenHeight = 0;      // Height of the screen in pixels
+int gScreenWidth = 0;            // Width of the screen in pixels
+int gScreenHeight = 0;           // Height of the screen in pixels
 uint32_t gScreenTotalPixels = 0; // Total pixels in the screen
 
-#define gHalfScreenWidth (gScreenWidth >> 1)  // Half the screen width in pixels
-#define gHalfScreenHeight (gScreenHeight >> 1)  // Half the screen height in pixels
+#define gHalfScreenWidth (gScreenWidth >> 1)   // Half the screen width in pixels
+#define gHalfScreenHeight (gScreenHeight >> 1) // Half the screen height in pixels
 
 // Corrected pixel width for handling SDL screen pitch
-uint32_t gCorrectPixelsWidth = 0; // Number of pixels per row in framebuffer
+int gCorrectPixelsWidth = 0; // Number of pixels per row in framebuffer
 
 #endif
