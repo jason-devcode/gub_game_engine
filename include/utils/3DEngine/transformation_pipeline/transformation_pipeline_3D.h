8c3e618f1e23f165a12c4cfa1661c189a3fc63d1
jason-devcode <jheisontoro62@gmail.com>
2024-08-05 13:50:28 -0500

    feat: added transformation pipeline header files

---
+++ include/utils/3DEngine/transformation_pipeline/transformation_pipeline_3D.h
@@ -0,0 +1,11 @@
+#ifndef TRANSFORMATION_PIPELINE_3D_H
+#define TRANSFORMATION_PIPELINE_3D_H
+
+#include "phase_1_local_space.h"
+#include "phase_2_world_space.h"
+#include "phase_3_view_space.h"
+#include "phase_4_homogeneous_clip_space.h"
+#include "phase_5_normalised_device_space.h"
+#include "phase_6_viewport_space.h"
+
+#endif
