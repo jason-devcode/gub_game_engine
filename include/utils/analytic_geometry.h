ed6a12f475cea6ad11edcd5c5cafaf8ab29fff25
jason-devcode <jheisontoro62@gmail.com>
2024-08-06 03:14:11 -0500

    fix: fixed triangle area

--- include/utils/analytic_geometry.h
+++ include/utils/analytic_geometry.h
@@ -50,6 +50,6 @@
  * @return Area of the triangle formed by the three vertices.
  */
 #define calculateTriangleAreaFromCoords(x1, y1, x2, y2, x3, y3) \
-    (calculateParallelogramAreaFromCoords((x2) - (x1), (y2) - (y1), (x3) - (x1), (y3) - (y1)) / 2)
+    (calculateParallelogramAreaFromCoords((x2) - (x1), (y2) - (y1), (x3) - (x1), (y3) - (y1)))
 
 #endif
