daf608a863a4e9dda8e1469f388bf3ef25073729
jason-devcode <jheisontoro62@gmail.com>
2024-08-09 14:28:03 -0500

    update: update build

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ if [ ! -d "build" ]; then
     mkdir build
 fi
 
-gcc src/sample_cube.c -o build/sample -lm -lSDL -O3
+# gcc src/sample_fast_triangle.c -o build/sample -lm -lSDL -O3
+# gcc src/sample_triangle.c -o build/sample -lm -lSDL -O3
+gcc src/sample_render_mesh.c -o build/sample -lm -lSDL -O3
 
 echo "Compilation completed."
