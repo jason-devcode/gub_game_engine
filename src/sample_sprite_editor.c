9293eee0063ec19db603be3fcb64345207f2ad1d
jason-devcode <jheisontoro62@gmail.com>
2024-08-09 14:31:47 -0500

    feat: in process sprite editor

---
+++ src/sample_sprite_editor.c
@@ -0,0 +1,40 @@
+#include "../include/game_engine.h"
+
+#include "../include/utils/color.h"
+#include "../include/utils/geometry.h"
+#include "../include/utils/color_palette.h"
+
+typedef struct _tag_color_picker_component_
+{
+    int x, y, cellWidth, cellHeight;
+    int colorSelected, shadeSelected;
+} ColorPicker;
+
+ColorPicker *createColorPicker(int x, int y, int cellWidth, int cellHeight)
+{
+    ColorPicker *colorPicker = (ColorPicker *)malloc(sizeof(ColorPicker));
+    colorPicker->x = x;
+    colorPicker->y = y;
+    colorPicker->cellWidth = cellWidth;
+    colorPicker->cellHeight = cellHeight;
+    return colorPicker;
+}
+
+void drawColorPicker(ColorPicker *colorPicker)
+{
+}
+
+void *gameLoop(void *ignore)
+{
+    do
+    {
+        clearScreen();
+
+        drawScreen();
+        renderDelay(16);
+    } while (ON_GAME_RUNNING);
+
+    return NULL;
+}
+
+CREATE_GAME(640, 640, "Sprite Editor");
