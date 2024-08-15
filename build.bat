30cb0b164239d828daeb741c25210c2f937aaa0d
jason-devcode <jheisontoro62@gmail.com>
2024-08-15 14:27:09 -0500

    feat: update build script windows

--- build.bat
+++ build.bat
@@ -1,4 +1,9 @@
 @echo off
+
+if not exist "build" (
+    mkdir "build"
+)
+
 @REM gcc src/sample_game_engine_api.c -o build/sample.exe -D USE_SDL_2 -D SDL_MAIN_HANDLED -lSDL2 -O3 --pedantic -Wfatal-errors
 @REM gcc src/sample_keyboard_events.c -o build/sample.exe -D USE_SDL_2 -D SDL_MAIN_HANDLED -lSDL2 -O3 --pedantic -Wfatal-errors
 gcc src/sample_mouse_events.c -o build/sample.exe -D USE_SDL_2 -D SDL_MAIN_HANDLED -lSDL2 -O3 --pedantic -Wfatal-errors
